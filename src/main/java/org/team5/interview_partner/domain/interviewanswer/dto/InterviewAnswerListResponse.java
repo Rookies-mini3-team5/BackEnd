cb0ebceeea72c2a7545ed3545b6c242e4bf340fd
민석 <47300870+seok019283501@users.noreply.github.com>
2024-09-24 17:51:04 +0900

    feat: 답변 피드백 목록 조회

--- src/main/java/org/team5/interview_partner/domain/interviewanswer/dto/InterviewAnswerListResponse.java
+++ src/main/java/org/team5/interview_partner/domain/interviewanswer/dto/InterviewAnswerListResponse.java
@@ -1,7 +1,9 @@
 package org.team5.interview_partner.domain.interviewanswer.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
