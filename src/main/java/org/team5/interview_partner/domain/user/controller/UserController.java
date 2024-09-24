cc49367daba61394f49b451f6cf207507f6de902
Daviswhistle <46082741+Daviswhistle@users.noreply.github.com>
2024-09-24 18:24:14 +0900

    프로필 조회 구현

--- src/main/java/org/team5/interview_partner/domain/user/controller/UserController.java
+++ src/main/java/org/team5/interview_partner/domain/user/controller/UserController.java
@@ -7,12 +7,22 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.team5.interview_partner.common.api.Api;
+import org.team5.interview_partner.common.utils.JwtUtils;
+import org.team5.interview_partner.domain.user.dto.UserInfoResponse;
+import org.team5.interview_partner.domain.user.service.UserService;
+import org.team5.interview_partner.entity.user.UserPictureRepository;
+import org.team5.interview_partner.entity.user.UserRepository;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping
 @Slf4j
 public class UserController {
+    private final UserService userService;
+    private final JwtUtils jwtUtils;
+    private final UserRepository userRepository;
+    private final UserPictureRepository userPictureRepository;
+
     @GetMapping("/mypage")
     public Api<UserInfoResponse> getProfile(
             @RequestHeader("Authorization") String authorization
