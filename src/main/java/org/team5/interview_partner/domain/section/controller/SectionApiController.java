fc6f6052671d96e387e0fc5df7d717cc40bba3bc
Daviswhistle <46082741+Daviswhistle@users.noreply.github.com>
2024-09-25 18:17:35 +0900

    section 생성 및 직군/직무 DB 구현

--- src/main/java/org/team5/interview_partner/domain/section/controller/SectionApiController.java
+++ src/main/java/org/team5/interview_partner/domain/section/controller/SectionApiController.java
@@ -2,16 +2,13 @@ package org.team5.interview_partner.domain.section.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.team5.interview_partner.common.api.Api;
 import org.team5.interview_partner.common.utils.JwtUtils;
+import org.team5.interview_partner.domain.section.dto.AddSectionRequest;
+import org.team5.interview_partner.domain.section.dto.AddSectionResponse;
 import org.team5.interview_partner.domain.section.dto.SectionInfoResponse;
 import org.team5.interview_partner.domain.section.service.SectionService;
-import org.team5.interview_partner.domain.user.dto.UserInfoResponse;
-import org.team5.interview_partner.domain.user.service.UserService;
 import org.team5.interview_partner.entity.user.UserRepository;
 
 import java.util.List;
@@ -31,5 +28,14 @@ public class SectionApiController {
     ){
         List<SectionInfoResponse> sectionInfoResponseList = sectionService.sectionInfoList(authorization);
         return Api.OK(sectionInfoResponseList);
+    }
+
+    @PostMapping("/section")
+    public Api<AddSectionResponse> addSection(
+            @RequestHeader("Authorization") String authorization,
+            @RequestBody AddSectionRequest addSectionRequest
+    ){
+        AddSectionResponse addSectionResponse = sectionService.addSection(authorization, addSectionRequest);
+        return Api.OK(addSectionResponse);
     }
 }
---
+++ src/main/java/org/team5/interview_partner/domain/section/dto/AddSectionRequest.java
@@ -0,0 +1,9 @@
+package org.team5.interview_partner.domain.section.dto;
+
+import lombok.Data;
+
+@Data
+public class AddSectionRequest {
+    private Integer occupational;
+    private Integer job;
+}
