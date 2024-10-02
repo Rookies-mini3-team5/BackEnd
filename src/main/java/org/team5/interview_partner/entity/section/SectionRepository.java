9d831b5fe199fbf920eb0af172b01f3b82a520f1
Daviswhistle <46082741+Daviswhistle@users.noreply.github.com>
2024-10-02 12:20:10 +0900

    section 이름 생성 로직 수정

--- src/main/java/org/team5/interview_partner/entity/section/SectionRepository.java
+++ src/main/java/org/team5/interview_partner/entity/section/SectionRepository.java
@@ -14,5 +14,5 @@ public interface SectionRepository extends JpaRepository<SectionEntity,Integer> 
     Optional<SectionEntity> findByName(String name);
 
     // 주어진 name 값이 prefix로 시작하는 모든 Section을 찾는 메서드
-    List<SectionEntity> findAllByNameStartingWith(String name);
+    List<SectionEntity> findAllByUserAndNameStartingWith(UsersEntity user, String name);
 }
